d3ff1e35b82aad04adb9ed446965131339df5622
minuki <alsdnr0315@gmail.com>
2020-08-24 22:07:44 +0900

    router test

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/Xxx',
+    name: 'Xxx',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "Xxx" */ '../views/Xxx.vue')
   }
+
 ]
 
 const router = new VueRouter({
